79f7a27188fcecfb29a8755852134a73bb4d3691
YisusCrz <tioyisu@gmail.com>
2020-10-09 14:01:10 -0700

    Initial commit

---
+++ bin/peticionhttp.dart
@@ -0,0 +1,5 @@
+import 'package:peticionhttp/peticionhttp.dart' as peticionhttp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${peticionhttp.calculate()}!');
+}
---
+++ test/peticionhttp_test.dart
@@ -0,0 +1,8 @@
+import 'package:peticionhttp/peticionhttp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
